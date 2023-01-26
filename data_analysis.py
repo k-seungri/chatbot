562aa31dbcc50d0586497819ff52492df9d1937c
Seungri Kang <sr930315@gmail.com>
2023-01-26 17:15:11 +0900

    20230126 - 데이터분석

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,135 @@
 import pandas as pd
+from konlpy.tag import Okt
+import matplotlib.pyplot as plt
+import numpy as np
 
 DATA_IN_PATH = './data_in/'
 
+## 파일 불러오기
 data = pd.read_csv(DATA_IN_PATH+'ChatBotData.csv', encoding='utf-8')
 
-print(data)
+## 문장 전체에 대한 분석
+# 데이터의 길이를 분석하기 위해 두 데이터를 하나의 리스트로 만듬
+sentences = list(data['Q']) + list(data['A'])
+
+# 음절 단위 길이 분석 ex) "자","연","어","처","리","공","부","는","매","우","어","렵","다"
+# 어절 단위 길이 분석 ex) "자연어","처리","공부는","매우","어렵다"
+# 형태소 단위 길이 분석 ex) "자연어","처리","공부","는","매우","어렵","다"
+
+# 띄어쓰기 기준으로 문장 나누기
+tokenized_sentences = [s.split() for s in sentences]
+# 어절의 길이 측정
+sent_len_by_token = [len(t) for t in tokenized_sentences]
+# 음절의 길이 측정
+sent_len_by_eumjeol = [len(s.replace(' ','')) for s in sentences]
+
+okt = Okt()
+# 형태소 분석
+morph_tokenized_sentences = [okt.morphs(s.replace(' ','')) for s in sentences]
+# 형태소 길이 측정
+sent_len_by_morph = [len(t) for t in morph_tokenized_sentences]
+
+plt.figure(figsize=(12,5))
+plt.hist(sent_len_by_token, bins=50, range=[0,50], alpha=0.5, color='r', label='eojeol')
+plt.hist(sent_len_by_morph, bins=50, range=[0,50], alpha=0.5, color='g', label='morph')
+plt.hist(sent_len_by_eumjeol, bins=50, range=[0,50], alpha=0.5, color='b', label='eumjeol')
+plt.title('Sentence Length Histogram')
+plt.xlabel('Sentence Length')
+plt.ylabel('Number of Sentences')
+# plt.show()
+
+# y값 조정
+plt.figure(figsize=(12,5))
+plt.hist(sent_len_by_token, bins=50, range=[0,50], alpha=0.5, color='r', label='eojeol')
+plt.hist(sent_len_by_morph, bins=50, range=[0,50], alpha=0.5, color='g', label='morph')
+plt.hist(sent_len_by_eumjeol, bins=50, range=[0,50], alpha=0.5, color='b', label='eumjeol')
+plt.yscale('log')
+plt.title('Sentence Length Histogram')
+plt.xlabel('Sentence Length')
+plt.ylabel('Number of Sentences')
+# plt.show()
+
+# 어절 통계값
+print('어절 최대길이: {}'.format(np.max(sent_len_by_token)))
+print('어절 최소길이: {}'.format(np.min(sent_len_by_token)))
+print('어절 평균길이: {:.2f}'.format(np.mean(sent_len_by_token)))
+print('어절 길이 표준편차: {:.2f}'.format(np.std(sent_len_by_token)))
+print('어절 중간길이: {}'.format(np.median(sent_len_by_token)))
+print('제 1 사분위 길이: {}'.format(np.percentile(sent_len_by_token, 25)))
+print('제 3 사분위 길이: {}'.format(np.percentile(sent_len_by_token, 75)))
+
+# 형태소 통계값
+print('형태소 최대길이: {}'.format(np.max(sent_len_by_morph)))
+print('형태소 최소길이: {}'.format(np.min(sent_len_by_morph)))
+print('형태소 평균길이: {:.2f}'.format(np.mean(sent_len_by_morph)))
+print('형태소 길이 표준편차: {:.2f}'.format(np.std(sent_len_by_morph)))
+print('형태소 중간길이: {}'.format(np.median(sent_len_by_morph)))
+print('형태소 1/4 퍼센타일 길이: {}'.format(np.percentile(sent_len_by_morph, 25)))
+print('형태소 3/4 퍼센타일 길이: {}'.format(np.percentile(sent_len_by_morph, 75)))
+
+# 음절 통계값
+print('음절 최대길이: {}'.format(np.max(sent_len_by_eumjeol)))
+print('음절 최소길이: {}'.format(np.min(sent_len_by_eumjeol)))
+print('음절 평균길이: {:.2f}'.format(np.mean(sent_len_by_eumjeol)))
+print('음절 길이 표준편차: {:.2f}'.format(np.std(sent_len_by_eumjeol)))
+print('음절 중간길이: {}'.format(np.median(sent_len_by_eumjeol)))
+print('음절 1/4 퍼센타일 길이: {}'.format(np.percentile(sent_len_by_eumjeol, 25)))
+print('음절 3/4 퍼센타일 길이: {}'.format(np.percentile(sent_len_by_eumjeol, 75)))
+
+
+plt.figure(figsize=(12, 5))
+plt.boxplot([sent_len_by_token, sent_len_by_morph, sent_len_by_eumjeol],
+            labels=['Eojeol', 'Morph', 'Eumjeol'],
+            showmeans=True)
+# plt.show()
+
+# 질문, 답변 각각에 대한 문장 길이 분포 분석
+query_sentences = list(data['Q'])
+answer_sentences = list(data['A'])
+
+query_morph_tokenized_sentences = [okt.morphs(s.replace(' ', '')) for s in query_sentences]
+query_sent_len_by_morph = [len(t) for t in query_morph_tokenized_sentences]
+
+answer_morph_tokenized_sentences = [okt.morphs(s.replace(' ', '')) for s in answer_sentences]
+answer_sent_len_by_morph = [len(t) for t in answer_morph_tokenized_sentences]
+
+plt.figure(figsize=(12, 5))
+plt.hist(query_sent_len_by_morph, bins=50, range=[0,50], color='g', label='Query')
+plt.hist(answer_sent_len_by_morph, bins=50, range=[0,50], color='r', alpha=0.5, label='Answer')
+plt.legend()
+plt.yscale('log', nonposy='clip')
+plt.title('Query Length Log Histogram by Morph Token')
+plt.xlabel('Query Length')
+plt.ylabel('Number of Queries')
+
+plt.figure(figsize=(12, 5))
+plt.boxplot([query_sent_len_by_morph, answer_sent_len_by_morph],
+            labels=['Query','Answer'],
+            showmeans=True)
+plt.show()
+
+
+# 데이터 어휘 빈도 분석
+query_NVA_token_sentences = list()
+answer_NVA_token_sentences = list()
+
+# 동사, 명사, 형용사를 제외한 나머지 문자 제거
+for s in query_sentences:
+    for token, tag in okt.pos(s.replace(' ', '')):
+        if tag == 'Noun' or tag == 'Verb' or tag == 'Adjective':
+            query_NVA_token_sentences.append(token)
+
+for s in answer_sentences:
+    temp_token_bucket = list()
+    for token, tag in okt.pos(s.replace(' ', '')):
+        if tag == 'Noun' or tag == 'Verb' or tag == 'Adjective':
+            answer_NVA_token_sentences.append(token)
+
+query_NVA_token_sentences = ' '.join(query_NVA_token_sentences)
+answer_NVA_token_sentences = ' '.join(answer_NVA_token_sentences)
+
+query_wordcloud = WordCloud(font_path= DATA_IN_PATH + 'NanumGothic.ttf').generate(query_NVA_token_sentences)
+
+plt.imshow(query_wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
